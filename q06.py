061260575ae8f5132f1ae04ff5e5530f5b6e1082
GitCida <mariaaparecidaily2@gmail.com>
2024-08-19 17:38:45 -0300

    enviando questões, data: 19/08

---
+++ q06.py
@@ -0,0 +1,6 @@
+n = 0
+while n < 10:
+    n += 1
+    print(n)
+#for x in range (1,11,1):
+#    print(x)
---
+++ q08.py
@@ -0,0 +1,6 @@
+for x in range(5,-1,-1):
+    import time
+    print(x)
+    time.sleep(1)
+    if x == 0:
+        print('Seu tempo acabou, CORRA!!!')
